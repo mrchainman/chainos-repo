868000a5379c3d673a98355c7cbba7bcb8a76e92
David Cimadom <david.cimadom@zone35.de>
2022-09-08 10:29:45 +0200

    chainos-utils-git update

--- scripts/repo.sh
+++ scripts/repo.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 cd ../ &&
-for i in pkgs/*/*.pkg.tar.zst
-do
-	name=$(echo $i | awk -F"/" '{print $NF}') &&
-	cp ./$i ./repo/ &&
-	cp ./$i.sig ./repo/ &&
+if [ $# -lt 1 ]
+then
+	for i in pkgs/*/*.pkg.tar.zst
+	do
+		name=$(echo $i | awk -F"/" '{print $NF}') &&
+		cp ./$i ./repo/ &&
+		cp ./$i.sig ./repo/ &&
+		repo-add ./repo/chainos-repo.db.tar.xz ./repo/$name
+	done
+	rm repo/chainos-repo.db repo/chainos-repo.files
+	mv repo/chainos-repo.db.tar.xz repo/chainos-repo.db
+	mv repo/chainos-repo.files.tar.xz repo/chainos-repo.files
+else
+	name=$(echo $1 | awk -F"/" '{print $NF}') &&
+	cp ./$1 ./repo/ &&
+	cp ./$1.sig ./repo/ &&
 	repo-add ./repo/chainos-repo.db.tar.xz ./repo/$name
-done
-rm repo/chainos-repo.db repo/chainos-repo.files
-mv repo/chainos-repo.db.tar.xz repo/chainos-repo.db
-mv repo/chainos-repo.files.tar.xz repo/chainos-repo.files
+	rm repo/chainos-repo.db repo/chainos-repo.files
+	mv repo/chainos-repo.db.tar.xz repo/chainos-repo.db
+	mv repo/chainos-repo.files.tar.xz repo/chainos-repo.files
+fi
