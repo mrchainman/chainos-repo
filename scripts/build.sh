45e06ec54227d9434c7e18cf904403fb8dfe56cc
David Cimadom <david.cimadom@zone35.de>
2022-09-11 16:07:29 +0200

    update + scripts improvement

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,28 @@
 cd ../pkgs &&
 if [ $# -lt 1 ]
 then
-	for i in ./*
-	do
-		cd $i && makepkg -c -C -f --sign --key F818840FB1CDD7C4CCAB06A41F08899E4EB9D50B && cd ..|| cd ..
-	done
+	printf "
+Usage:\n
+./build.sh all - Build all packages\n
+./build.sh git - Build all git packages\n
+./build.sh <pkg> - Build <pkg>\n
+	"
 else
-	cd $1 && makepkg -c -C -f --sign --key F818840FB1CDD7C4CCAB06A41F08899E4EB9D50B
+	case $1 in
+		all)
+			for i in ./*
+			do
+				cd $i && makepkg -c -C -f --sign --key F818840FB1CDD7C4CCAB06A41F08899E4EB9D50B && cd ..|| cd ..
+			done
+			;;
+		git)
+			for i in ./*-git
+			do
+				cd $i && makepkg -c -C -f --sign --key F818840FB1CDD7C4CCAB06A41F08899E4EB9D50B && cd ..|| cd ..
+			done
+			;;
+		*)
+			cd $1 && makepkg -c -C -f --sign --key F818840FB1CDD7C4CCAB06A41F08899E4EB9D50B
+			;;
+	esac
 fi
-
--- scripts/repo.sh
+++ scripts/repo.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 cd ../ &&
-if [ $# -lt 1 ]
-then
-	for i in pkgs/*/*.pkg.tar.zst
-	do
-		name=$(echo $i | awk -F"/" '{print $NF}') &&
-		cp ./$i ./repo/ &&
-		cp ./$i.sig ./repo/ &&
-		repo-add ./repo/chainos-repo.db.tar.xz ./repo/$name
-	done
-	rm repo/chainos-repo.db repo/chainos-repo.files
-	mv repo/chainos-repo.db.tar.xz repo/chainos-repo.db
-	mv repo/chainos-repo.files.tar.xz repo/chainos-repo.files
-else
-	name=$(echo $1 | awk -F"/" '{print $NF}') &&
-	cp ./$1 ./repo/ &&
-	cp ./$1.sig ./repo/ &&
+for i in pkgs/*/*.pkg.tar.zst
+do
+	name=$(echo $i | awk -F"/" '{print $NF}') &&
+	cp ./$i ./repo/ &&
+	cp ./$i.sig ./repo/ &&
 	repo-add ./repo/chainos-repo.db.tar.xz ./repo/$name
-	rm repo/chainos-repo.db repo/chainos-repo.files
-	mv repo/chainos-repo.db.tar.xz repo/chainos-repo.db
-	mv repo/chainos-repo.files.tar.xz repo/chainos-repo.files
-fi
+done
+rm repo/chainos-repo.db repo/chainos-repo.files
+mv repo/chainos-repo.db.tar.xz repo/chainos-repo.db
+mv repo/chainos-repo.files.tar.xz repo/chainos-repo.files
